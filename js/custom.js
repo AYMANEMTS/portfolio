ace54510e670bd45d74f0997ac2c801db88d39ac
aymane <oujdimraymane@gmail.com>
2024-10-03 00:02:05 +0100

    contact submition

--- js/custom.js
+++ js/custom.js
@@ -34,3 +34,33 @@
     $('.social-links a').tooltip();
 
 })(jQuery);
+
+function ContactForm(e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  const data = { name, email, message };
+
+  fetch('https://formspree.io/f/mrbznvlw', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+    mode: 'cors'
+  })
+  .then(response => {
+    if (response.ok) {
+      alert("Success! Your message has been sent. We will get back to you shortly.");
+      window.location.reload();
+    } else {
+      alert("There was a problem submitting your form. Please try again later.");
+    }
+  })
+  .catch(error => {
+    console.error('Failed to submit form: ', error);
+  });
+}
